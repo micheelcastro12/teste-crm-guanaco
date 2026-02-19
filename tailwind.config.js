a6ac815c697b0a31d43c12b5859edf88d04d06ca
micheelcastro12 <micheelcastro12@gmail.com>
2026-02-19 17:07:24 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: "#4318FF",
+        secondary: "#A3AED0",
+        brand: {
+          50: "#F4F7FE",
+          500: "#4318FF",
+          700: "#1B2559",
+        }
       },
     },
   },
   plugins: [],
-};
+}
